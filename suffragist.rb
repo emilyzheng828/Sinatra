4eef87d0893df78ff87594a0a5b24368a9033f69
Emily Zheng <emily.zheng828@gmail.com>
2015-04-01 21:03:53 +0100

    cast vote

--- suffragist.rb
+++ suffragist.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 get '/' do
+   @title = 'Welcome to the Suffragist!'
    erb :index
 end
    Choices = {
@@ -9,3 +10,15 @@ end
   'NOO' => 'Noodles',
 }
 
+post '/cast' do
+  @title = 'Thanks for casting your vote!'
+  @vote = params['vote']
+  erb :cast
+end
+
+get '/results' do
+  @votes = { 'waw' => 7, 'krk' => 5 }
+  erb :results
+end
+
+  
